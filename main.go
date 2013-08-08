10f83e3844e3b233098d795539334d250fe73d71
Lunny Xiao <xiaolunwen@gmail.com>
2013-08-08 14:02:50 +0800

    remove import .

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"io/ioutil"
 
 	_ "github.com/go-sql-driver/mysql"
-	. "github.com/lunny/xorm"
-	. "github.com/lunny/xweb"
+	"github.com/lunny/xorm"
+	"github.com/lunny/xweb"
 )
 
 const APP_VER = "0.0.1 Beta"
@@ -26,10 +26,10 @@ func main() {
 		return
 	}
 
-	cfgs := SimpleParse(string(data))
+	cfgs := xweb.SimpleParse(string(data))
 
 	// create Orm
-	actions.Orm, err = NewEngine("mysql", fmt.Sprintf("%v:%v@%v/%v?charset=utf8",
+	actions.Orm, err = xorm.NewEngine("mysql", fmt.Sprintf("%v:%v@%v/%v?charset=utf8",
 		cfgs["dbuser"], cfgs["dbpasswd"], cfgs["dbhost"], cfgs["dbname"]))
 	if err != nil {
 		fmt.Println(err)
@@ -38,14 +38,14 @@ func main() {
 	actions.Orm.ShowSQL = true
 
 	// add actions
-	AddAction(&actions.HomeAction{})
-	AddRouter("/exercise", &actions.ExerciseAction{})
-	AddRouter("/question", &actions.QuestionAction{})
-	AddAction(&actions.UserAction{})
+	xweb.AddAction(&actions.HomeAction{})
+	xweb.AddRouter("/exercise", &actions.ExerciseAction{})
+	xweb.AddRouter("/question", &actions.QuestionAction{})
+	xweb.AddAction(&actions.UserAction{})
 
 	// add login filter
-	app := MainServer().RootApp
-	loginFilter := NewLoginFilter(app, actions.USER_ID_TAG, "/login")
+	app := xweb.MainServer().RootApp
+	loginFilter := xweb.NewLoginFilter(app, actions.USER_ID_TAG, "/login")
 	loginFilter.AddAnonymousUrls("/", "/exercise/", "/exercise/compile",
 		"/login", "/about")
 	app.AddFilter(loginFilter)
@@ -58,5 +58,5 @@ func main() {
 	app.SetConfig("Orm", actions.Orm)
 
 	// run the web server
-	Run(fmt.Sprintf("%v:%v", cfgs["address"], cfgs["port"]))
+	xweb.Run(fmt.Sprintf("%v:%v", cfgs["address"], cfgs["port"]))
 }
